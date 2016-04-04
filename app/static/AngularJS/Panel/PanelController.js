d45de0a6bc8711e0b4364b9de7450bd83a5e3383
falvaradoluna <falvaradoluna@gmail.com>
2016-04-04 10:42:29 -0500

    FAL
    Windows modal

--- app/static/AngularJS/Panel/PanelController.js
+++ app/static/AngularJS/Panel/PanelController.js
@@ -1,15 +1,16 @@
 // -- =============================================
 // -- Author:      Fernando Alvarado Luna
 // -- Create date: 01/04/2016
-// -- Description: Carrusel de aplicaciones
+// -- Description: Carrusel de aplicaciones controller con función que trae 
 // -- Modificó: 
 // -- Fecha: 
 // -- =============================================
-registrationModule.controller('panelController', function($scope, alertFactory, panelRepository){
+registrationModule.controller('panelController', function($scope, alertFactory, panelRepository,$window){
 	
 	$scope.idUsuario = 1;
 	$scope.init = function(){
 
+		//FAL trae aplicaciones por usuario
     	panelRepository.getAplicaciones($scope.idUsuario)
     		.then(function successCallback(response) {
 			    $scope.Aplicaciones = response.data;
@@ -20,7 +21,7 @@ registrationModule.controller('panelController', function($scope, alertFactory, 
 			    alertFactory.error('Error al obtener las aplicaciones.');
   			}
   		);
-
+    	//FAL trae datos del usuario
     	panelRepository.getUsuarios($scope.idUsuario)
     		.then(function successCallback(response) {
 			    $scope.empleado = response.data;
@@ -33,4 +34,8 @@ registrationModule.controller('panelController', function($scope, alertFactory, 
   		);
 	}
 
+	$scope.openWindow = function(url,titulo) {
+		url= "http://" + url;
+        $window.open(url, titulo, 'width=800,height=600');
+    };
 });
