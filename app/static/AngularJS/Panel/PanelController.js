66dde861983acc136789b7e73fc81a94e69c4a63
lourdesmaldonado <lou_mald@hotmail.com>
2016-10-14 11:32:09 -0500

    Menu Lateral

--- app/static/AngularJS/Panel/PanelController.js
+++ app/static/AngularJS/Panel/PanelController.js
@@ -5,55 +5,53 @@
 // -- Modificó: 
 // -- Fecha: 
 // -- =============================================
-registrationModule.controller('panelController', function($scope, alertFactory, panelRepository,$window, $location){
+registrationModule.controller('panelController', function ($scope, $rootScope, alertFactory, panelRepository, $window, $location) {
 
-	//Función de Inicio
-	$scope.init = function(){
+    //Función de Inicio
+    $scope.init = function () {
 
-		$scope.idUsuario = $('#lgnUser').val();
+        $rootScope.idUsuario = $('#lgnUser').val();
 
-		//FAL trae aplicaciones por usuario
-    	panelRepository.getAplicaciones($scope.idUsuario)
-    		.then(function successCallback(response) {
-			    $scope.Aplicaciones = response.data;
-			    setTimeout(function(){ 
-			    	 $('#dg-container').gallery({
-						autoplay: true
-					 });
-			    }, 1);
-  			}, function errorCallback(response) {
-			    alertFactory.error('Error al obtener las aplicaciones.');
-  			}
-  		);
+        //FAL trae aplicaciones por usuario
+        panelRepository.getAplicaciones($rootScope.idUsuario)
+            .then(function successCallback(response) {
+                $rootScope.Aplicaciones = response.data;
+                setTimeout(function () {
+                    $('#dg-container').gallery({
+                        autoplay: true
+                    });
+                }, 1);
+            }, function errorCallback(response) {
+                alertFactory.error('Error al obtener las aplicaciones.');
+            });
 
-    	
-    	//FAL trae datos del usuario
-    	panelRepository.getUsuarios($scope.idUsuario)
-    		.then(function successCallback(response) {
-			    $scope.empleado = response.data;
 
-  			}, function errorCallback(response) {
-			    alertFactory.error('Error al obtener los datos del usuario.');
-  			}
-  		);
+        //FAL trae datos del usuario
+        panelRepository.getUsuarios($rootScope.idUsuario)
+            .then(function successCallback(response) {
+                $scope.empleado = response.data;
 
-    	
-	}
+            }, function errorCallback(response) {
+                alertFactory.error('Error al obtener los datos del usuario.');
+            });
 
-	$scope.openWindow = function(url,titulo) {
-		var form = document.createElement("form");
-		form.setAttribute("method", "post");
-		form.setAttribute("action", url);
 
-		form.setAttribute("target", titulo);
+    }
 
-		var hiddenField = document.createElement("input"); 
-		hiddenField.setAttribute("type", "hidden");
-		hiddenField.setAttribute("name", "idUsuario");
-		hiddenField.setAttribute("value", $scope.idUsuario);
-		form.appendChild(hiddenField);
-		document.body.appendChild(form);
-		window.open('', titulo);
-		form.submit();
+    $scope.openWindow = function (url, titulo) {
+        var form = document.createElement("form");
+        form.setAttribute("method", "post");
+        form.setAttribute("action", url);
+
+        form.setAttribute("target", titulo);
+
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", "idUsuario");
+        hiddenField.setAttribute("value", $rootScope.idUsuario);
+        form.appendChild(hiddenField);
+        document.body.appendChild(form);
+        window.open('', titulo);
+        form.submit();
     };
 });
